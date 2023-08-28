ee057304a9257ec96e6360199708fa5a58f8ae53
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-08-28 22:19:23 +0300

    Define your User and Message models

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key' # Change this to secret key later
@@ -12,6 +13,17 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'  # Set the login view function
 
 # TODO: Define database models
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 # TODO: Define API endpoints
 
