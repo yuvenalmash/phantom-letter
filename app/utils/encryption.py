2af371c63da0fe633fbdcc3be423b1e72deed1f0
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-10-17 23:53:08 +0300

    Add functions to encrypt and decrypt messages

--- app/utils/encryption.py
+++ app/utils/encryption.py
@@ -15,3 +15,13 @@ def load_key_from_file(filename, passphrase):
     with open(filename, 'rb') as file:
         key = RSA.import_key(file.read(), passphrase=passphrase)
         return key
+    
+def rsa_encrypt(public_key, message):
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted_message = cipher.encrypt(message.encode())
+    return base64.b64encode(encrypted_message).decode()
+
+def rsa_decrypt(private_key, encrypted_message):
+    cipher = PKCS1_OAEP.new(private_key)
+    decrypted_message = cipher.decrypt(base64.b64decode(encrypted_message))
+    return decrypted_message.decode()
